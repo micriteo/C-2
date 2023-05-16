e04c9459b6c179acbfd456f47533b595ff178b3a
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2023-05-16 16:05:10 +0200

    Path class Created.

---
+++ C#2Game_Enemy_Test2/C#2Game_Enemy_Test2/Path.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_2Game_Enemy_Test2
+{
+    public class Path
+    {
+        public List<Vector2> Waypoints { get; private set; }
+
+        public Path(List<Vector2> waypoints)
+        {
+            this.Waypoints = waypoints;
+        }
+
+        
+    }
+    
+}
