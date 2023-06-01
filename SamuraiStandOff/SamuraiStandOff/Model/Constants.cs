4bbcf5dbaaefadd209ca1c37257c50835061f569
Mathew Shardin <mathew.shardin@student.nhlstenden.com>
2023-06-01 15:25:10 +0200

    Added enemy costs

--- SamuraiStandOff/SamuraiStandOff/Model/Constants.cs
+++ SamuraiStandOff/SamuraiStandOff/Model/Constants.cs
@@ -10,5 +10,11 @@ namespace C_2Game_Enemy_Test2
     {
         public const int meleePrice = 100;
         public const int startBalance = 200;
+
+        // Enemy stats
+        public const int enemyMeleeCost = 50;
+        public const int enemySpeedCost = 25;
+        public const int enemyTankCost = 200;
+
     }
 }
